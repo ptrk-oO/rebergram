ed401024ae95bce565bb43f12449a676ea0e272f
ptrktws <patrick@ptrktwss-Air.fritz.box>
2021-11-21 00:51:19 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import stateMachine from './stateMachine.js';
+
+  var StateMachine = require('javascript-state-machine');
+  var visualize = require('javascript-state-machine/lib/visualize');
+  var fsm = new StateMachine({
+    init: 'closed',
+    transitions: [
+      { name: 'open',  from: 'closed', to: 'open',   dot: { color: 'blue', headport: 'n', tailport: 'n' } },
+      { name: 'close', from: 'open',   to: 'closed', dot: { color: 'red',  headport: 's', tailport: 's' } }
+    ]
+  });
 
 function App() {
   return (
-	<stateMachine />
+	visualize(fsm)
   );
 }
 
